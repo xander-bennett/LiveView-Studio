c378b657a87efc5e1fd1a64c04f35cc1e59fa89f
Xander Bennett <47325573+xander-bennett@users.noreply.github.com>
2025-01-17 12:53:04 -0800

    adding starter files

---
+++ lib/live_view_studio_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule LiveViewStudioWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use LiveViewStudioWeb, :html
+
+  embed_templates "page_html/*"
+end
